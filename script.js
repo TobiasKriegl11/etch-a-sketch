9360e3414c1b8c56610e2ad25bbb7434dd7b187d
TobiasKriegl11 <241426819+TobiasKriegl11@users.noreply.github.com>
2026-06-20 17:56:13 +0200

    Add hover effect
    Chenge grid color when mouse passes over it

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ for (let i = 0; i < (16*16); i++) {
     const box = document.createElement('div');
     box.classList.add("square-box");
     squareContainer.appendChild(box);
-    console.log("Total boxes created: ", squareContainer.children.length);
+
+    box.addEventListener('mouseenter', () => {
+    box.style.backgroundColor = "black";
+    });
 };
+
+
+
