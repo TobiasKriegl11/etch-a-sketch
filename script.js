44166d982f3e32fa344f069ac77a43335c163482
TobiasKriegl11 <241426819+TobiasKriegl11@users.noreply.github.com>
2026-06-21 07:19:20 +0200

    feat: implement dynamic grid resizing
    - Wrap grid generation logic into
      a reusable `buildGrid` function
    - Add input validation to prevent grid sizes
      over 64, under 1 or invalid inputs
    - Dynamically calculate grid box sizing
      via inline style injection
    - Adjust HTML/CSS to account for button

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const squareContainer = document.getElementById('square-container');
+const buttonNewGrid = document.getElementById('new-grid');
 
-for (let i = 0; i < (16*16); i++) {
-    const box = document.createElement('div');
-    box.classList.add("square-box");
-    squareContainer.appendChild(box);
+let gridSize = 16;
 
-    box.addEventListener('mouseenter', () => {
-    box.style.backgroundColor = "black";
-    });
+function buildGrid(gridSize) {    
+    squareContainer.replaceChildren();
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+      const box = document.createElement('div');
+      box.classList.add("square-box");
+
+      box.style.width = `calc(100% / ${gridSize})`;
+      box.style.height = `calc(100% / ${gridSize})`;
+
+      squareContainer.appendChild(box);
+
+      box.addEventListener('mouseenter', () => {
+      box.style.backgroundColor = "black";
+      });
+    };
 };
+buildGrid(gridSize);
 
-
-
+buttonNewGrid.addEventListener('click', () => {
+    let userInput = prompt("Choose your grid size (1-64):")
+    gridSize = parseInt(userInput);
+    if (gridSize > 64 || gridSize < 1 ||isNaN(gridSize)) {
+        alert("Invalid input! Please enter a number between 1 and 64.")
+        return;
+    }
+    buildGrid(gridSize);
+});
